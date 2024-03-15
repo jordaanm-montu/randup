65ec42eaeb6fc31525b598ff8f85891ac0e7e8af
Jordaan Mylonas <jordaan.m@montu.com.au>
2024-03-15 10:34:29 +1000

    feat: styling

---
+++ src/data.ts
@@ -0,0 +1,12 @@
+export const paletteColors = [
+  "#9e0142",
+  "#d53e4f",
+  "#f46d43",
+  "#fdae61",
+  "#fee08b",
+  "#e6f598",
+  "#abdda4",
+  "#66c2a5",
+  "#3288bd",
+  "#5e4fa2",
+] as const;
