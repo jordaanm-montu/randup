6c9b1c9cd6d1715abc2c571ef14c745cf312c342
Jordaan Mylonas <jordaan.m@montu.com.au>
2024-11-06 16:57:13 +1000

    feat: add preset switch on main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { ThemeProvider } from "@emotion/react";
 import { Page } from "./components/Page";
 import { ListData } from "./contexts/list-data-context";
 import { PresetData } from "./contexts/preset-data.context";
+import { createTheme } from "@mui/material";
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
   return (
-    <PresetData>
-      <ListData>
-        <Page />
-      </ListData>
-    </PresetData>
+    <ThemeProvider theme={darkTheme}>
+      <PresetData>
+        <ListData>
+          <Page />
+        </ListData>
+      </PresetData>
+    </ThemeProvider>
   );
 }
 
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,10 +1,20 @@
-import { useContext, useState } from "react"
-import { ListDataContext } from "../contexts/list-data-context"
+import { useContext, useState } from "react";
+import { ListDataContext } from "../contexts/list-data-context";
 import { ListView } from "./List";
 
-import Button from '@mui/material/Button';
-import { styled, Box } from '@mui/material'
+import {
+  Box,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+  styled,
+} from "@mui/material";
+import Button from "@mui/material/Button";
 import { HiddenItemContext } from "../contexts/hidden-item.context";
+import { PresetDataContext } from "../contexts/preset-data.context";
 import { RandupLogo } from "./RandupLogo";
 import { SettingsDialog } from "./SettingsDialog/SettingsDialog";
 
@@ -12,7 +22,7 @@ const ActionsRow = styled(Box)`
   display: flex;
   gap: 1rem;
   justify-content: center;
-`
+`;
 
 const CardList = styled(Box)`
   margin: 1rem;
@@ -30,7 +40,7 @@ const CardList = styled(Box)`
       transition: all 0.2s;
 
       &.hidden {
-        background-color: #444!important;
+        background-color: #444 !important;
         color: #444;
         text-shadow: none;
       }
@@ -41,47 +51,79 @@ const CardList = styled(Box)`
       border-radius: 0.5rem;
       text-shadow: 1px 1px #222;
     }
-    
+
     .content.hidden + .link-container {
       visibility: hidden;
     }
   }
-`
+`;
 
 export const Page = () => {
   const listData = useContext(ListDataContext);
-  const {items, shuffle, unshuffle} = listData;
+  const presetData = useContext(PresetDataContext);
+  const { items, shuffle, unshuffle } = listData;
 
   const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
 
   const hiddenItemData = useContext(HiddenItemContext);
   const { hideItems, showAll } = hiddenItemData;
 
+  const presetNames = presetData.data.presets.map((preset) => preset.name);
+  const activePreset = presetData.data.activePreset;
+
   const openDialog = () => {
     setIsSettingsModalOpen(true);
-  }
+  };
 
   const hideAll = () => {
-    hideItems(items.map(x => x.id));
-  }
+    hideItems(items.map((x) => x.id));
+  };
 
   return (
-    <div style={{margin: '1rem'}}>
-      <div className='flex center'>
-        <RandupLogo/>
+    <div style={{ margin: "1rem" }}>
+      <div className="flex center">
+        <RandupLogo />
         <h1>Rand Up</h1>
       </div>
       <ActionsRow>
-        <Button variant='contained' onClick={() => shuffle()}>Shuffle</Button>
-        <Button variant='contained' onClick={() => unshuffle()}>Un-Shuffle</Button>
-        <Button variant='contained' onClick={openDialog}>Edit</Button>
-        <Button variant='contained' onClick={hideAll}>Hide All</Button>
-        <Button variant='contained' onClick={showAll}>Show All</Button>
+        <Button variant="contained" onClick={() => shuffle()}>
+          Shuffle
+        </Button>
+        <Button variant="contained" onClick={() => unshuffle()}>
+          Un-Shuffle
+        </Button>
+        <Button variant="contained" onClick={openDialog}>
+          Edit
+        </Button>
+        <Button variant="contained" onClick={hideAll}>
+          Hide All
+        </Button>
+        <Button variant="contained" onClick={showAll}>
+          Show All
+        </Button>
+        <FormControl variant="outlined" size="medium" color="secondary">
+          <Select
+            variant="standard"
+            color="primary"
+            value={activePreset}
+            onChange={(x) => presetData.setActivePreset(x.target.value)}
+          >
+            {presetNames.map((name) => (
+              <MenuItem value={name}>
+                <Typography>{name}</Typography>
+              </MenuItem>
+            ))}
+          </Select>
+          <FormHelperText>Change Preset</FormHelperText>
+        </FormControl>
       </ActionsRow>
       <CardList>
         <ListView />
       </CardList>
-      <SettingsDialog isOpen={isSettingsModalOpen} setIsOpen={setIsSettingsModalOpen} />
+      <SettingsDialog
+        isOpen={isSettingsModalOpen}
+        setIsOpen={setIsSettingsModalOpen}
+      />
     </div>
   );
-}
+};
